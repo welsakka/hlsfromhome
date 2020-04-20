b9b5db0e6a4ccadae1fd1dd4e261493d281e394f
welsakka <40042157+welsakka@users.noreply.github.com>
2020-04-20 15:53:38 -0400

    Add files via upload

---
+++ convolution.cpp
@@ -0,0 +1,32 @@
+/*
+ 
+As talked about in class today (4/14) modern neural networks used for image recognition perform many many convolutions.
+
+For the first assignment in this set of assignments, create a HLS valid (use optimization pragmas and interface
+pragmas as necessary) to create a convolution unit that will be able to compute the convolutions that 
+exist inside the first layer of AlexNet.
+
+The deliverable will be both your code and a small write up of any 
+optimization decisions/pragmas/interfaces you chose to use. The write-up should include why you chose
+them, the alternatives and why you believe the ones you choose were best suited for the task.
+
+Additionally, try to write a blurb about your the performance you would expect to see from your
+module assuming perfect conditions. (a.k.a how many cycles would you estimate it to take to run). Please also
+include the factors that you would assume and expect effect the performance your core.
+ 
+ */
+
+void convolution (int rgb[224][224][3]) {
+	
+	
+	int filter[11][11][3];
+	
+	//declare stride var?
+	
+	//do convolution??!?!?!?
+	
+	//do something with a stream
+	
+	return 
+	
+}
