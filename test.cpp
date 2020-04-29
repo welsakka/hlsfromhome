818a1257e65911a04ff132dbcd2e072fb492af0e
welsakka <40042157+welsakka@users.noreply.github.com>
2020-04-29 13:08:04 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
-#include "convolution.cpp"
+#include "conv.cpp"
 
 int main () {
 
-	int res[224][224];
-	int rgb[3][224][224];
-	int kernel[3][11][11];
+	//int res[224][224];
+	//int rgb[3][224][224];
+	//int kernel[3][11][11];
+
+	int res[50176]; //224 * 224
+	int rgb[150528]; // 224 * 224 * 3
+	int kernel[363]; //11 * 11 * 3
 
 	//initializations
-	for (int i = 0; i < 244; ++i) {
-		for (int j = 0; j < 244; ++j) {
-			for (int k = 0; k < 244; ++k) {
-				rgb[i][j][k] = 2;
-			}
-		}
+	for (int i = 0; i < 150528; ++i) {
+		#pragma HLS unroll
+		rgb[i] = 2;
 	}
-	for (int i = 0; i < 11; ++i) {
-		for (int j = 0; j < 11; ++j) {
-			for (int k = 0; k < 11; ++k) {
-				kernel[i][j][k] = 2;
-			}
-		}
+	for (int i = 0; i < 363; ++i) {
+		#pragma HLS unroll
+		kernel[i] = 2;
 	}
 
 
 	driver(rgb, kernel, res);
 
-	printf("yeet @ %d", res[0][0]);
+	printf("Value at (0, 0) = ", res[0][0]);
 
 	return 0;
 
